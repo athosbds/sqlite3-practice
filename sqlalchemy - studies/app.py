42c3e57d223ec4b90ae30606d5bb8cb0208eb0e7
Athos <69597077+athosbds@users.noreply.github.com>
2025-08-12 17:04:35 -0300

    crud basic

--- sqlalchemy - studies/app.py
+++ sqlalchemy - studies/app.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from . import app
+
 Base = declarative_base()
 
 class User(Base):
@@ -10,8 +10,26 @@ class User(Base):
     name = Column(String(50))
     age = Column(Integer)
 
-engine = create_engine('mysql+pymysql://root:root%40%2323@localhost/my_database') #CREATE DATABASE meu_banco;
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session
 
+engine = create_engine('mysql+pymysql://root:root%40%2323@localhost/my_database')
+Base.metadata.create_all(engine)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# CRUD
+def create_user(name, age):
+    new = User(name=name, age=age)
+    session.add(new)
+    session.commit()
+def list_users():
+    users = session.query(User).all()
+    for user in users:
+        print(f'ID: {user.id} - Nome: {user.name} - Idade: {user.age}')
+def delete_user(user):
+    user_delete = session.query(User).filter(user)
+    session.delete(user_delete)
+    session.commit()
+create_user('Augustin', 17)
+list_users()
