844cde3542e381ad1f20bbe2eb734172b6083208
Athos <69597077+athosbds@users.noreply.github.com>
2025-08-13 15:46:58 -0300

    Update app.py

--- sqlalchemy - studies/app.py
+++ sqlalchemy - studies/app.py
@@ -10,29 +10,32 @@ class User(Base):
     name = Column(String(50))
     age = Column(Integer)
 
-
-engine = create_engine('mysql+pymysql://root:rootlanguageauth2008@localhost/inventory_db')
+# Use placeholders ou dados fictícios
+engine = create_engine('mysql+pymysql://USUARIO:SENHA@HOST/NOME_DO_BANCO')
 Base.metadata.create_all(engine)
-
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
 # CRUD
 def create_user(name, age):
     new = User(name=name, age=age)
     session.add(new)
     session.commit()
+
 def list_users():
     users = session.query(User).all()
     for user in users:
         print(f'ID: {user.id} - Nome: {user.name} - Idade: {user.age}')
-def delete_user(user):
-    user_delete = session.query(User).filter(User.id == user).first()
+
+def delete_user(user_id):
+    user_delete = session.query(User).filter(User.id == user_id).first()
     if user_delete:
         session.delete(user_delete)
         session.commit()
     else:
-        print(f'Não Encontrado: {user}')
+        print(f'Não encontrado: {user_id}')
+
 def update_user(id_user, name=None, age=None):
     user_update = session.query(User).filter(User.id == id_user).first()
     if user_update:
@@ -44,8 +47,4 @@ def update_user(id_user, name=None, age=None):
     else:
         print(f'Usuário: {id_user} não encontrado - Nome: {name}')
 
-# create_user('Carlos', 46)
-# create_user('Eduarda', 20)
-# update_user(1, 'Carlos Eduardo', 23)
-# delete_user(1)
-list_users()
+# list_users()
